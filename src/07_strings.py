36345d76bca386b7e5417d007ae701816fba5d46
melody.lo <melody.lo@prontocloud.com.au>
2024-09-04 16:24:27 +1000

    update strings

--- src/07_strings.py
+++ src/07_strings.py
@@ -16,4 +16,27 @@ regex = re.compile(r"[\t ]+")
 result = regex.sub(" ", y)
 print(result)
 
-# backslashes can be used to escape characters
+# backslashes can be used to escape characters to give them special meanings
+# \n - newline
+# \t - tab
+# \\ - a single normal backslash character
+# \" - plain double-quote character
+z = "\tThis string starts with a \"tab\""
+k = "This string contains a single backslash(\\)"
+j = "This string has a newline \n NEW LINE"
+print(z)
+print(k)
+print(j)
+
+# single quote can be used
+i = "hello"
+m = 'hello'
+n = "don't need a backslash"
+o = 'can\'t get by without a backslash'
+p = 'you can have a double quote " in your string'
+q = """use double qoute " and
+single qoute ' and
+multiple lines
+"""
+print(i, m, n, o, p, q)
+
