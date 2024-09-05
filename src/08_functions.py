15c746b11df421cde6726ed8361525899adabe0e
roukell <62136980+roukell@users.noreply.github.com>
2024-09-05 10:25:24 +1000

    updated

--- src/08_functions.py
+++ src/08_functions.py
@@ -1,18 +1,18 @@
-def funct(x, y=1, z=1):
+def func(x, y=1, z=1):
     return x + 2 * y + z ** 2
 
 
-def funct1(x, y=1, z=1, *tup):
+def func1(x, y=1, z=1, *tup):
     print((x, y, z) + tup)
 
 
-def funct2(x, y=1, z=1, **kwargs):
+def func2(x, y=1, z=1, **kwargs):
     print(x, y, z, kwargs)
 
 
-# result = funct(3, z=5, y=0)
+# result = func(3, z=5, y=0)
 # print(result)
 
-# funct1(2, 0, 0, [3, 2, 1])
+# func1(2, 0, 0, [3, 2, 1])
 
-funct2(1, 0, 2, i=0, my_keyword="hello", my_keyword2="world")
+func2(1, 0, 2, i=0, my_keyword="hello", my_keyword2="world")
